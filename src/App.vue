fe859473ea5c9a657f2f79239c3f23bf71585a5d
Unrekoll <78980201+Unrekoll@users.noreply.github.com>
2021-05-18 21:40:33 +0300

    change text

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Sweet Home ALABAMABA</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">BOOBS</router-link>
     </div>
     <router-view/>
   </div>
